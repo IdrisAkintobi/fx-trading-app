f819f7c21318cf755f44d7ea21140e0dd4cecf1a
Idris Akintobi <akintobiidris@gmail.com>
2026-03-17 20:53:01 +0100

    Fix FxRatesModule dependency injection by importing AuthModule

--- src/modules/fx-rates/fx-rates.module.ts
+++ src/modules/fx-rates/fx-rates.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FxRatesService } from './fx-rates.service';
 import { FxRatesController } from './fx-rates.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [FxRatesController],
   providers: [FxRatesService],
   exports: [FxRatesService],
